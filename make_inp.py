630010f49e5c9dfffc3bb98e7a13abc5fd64722a
Geoffrey Tian <gtian@login1.maverick2.tacc.utexas.edu>
2020-03-29 18:38:49 -0500

    hw4q1 works?

--- make_inp.py
+++ make_inp.py
@@ -1,9 +1,14 @@
 import random
 random.seed(a = None, version=2)
 f = open("inp.txt", "w+")
+f.write("1000000,")
+min_num = 1000
 for i in range(1000000):
-	k = random.randint(0, 999)
+	k = random.randint(100, 999)
+	if k < min_num:
+		min_num = k
 	f.write(str(k))
 	f.write(",")
+print(min_num)
 f.write("900")
 
--- q1.cu
+++ q1.cu
@@ -3,7 +3,10 @@
 #include <string.h>
 #include <limits.h>
 
-
+#define NUM_THREADS_A 32
+#define NUM_BLOCKS_A 2
+#define NUM_THREADS_B 32
+#define NUM_BLOCKS_B 2
 int* fileToArray(char file1[], int* n){
   FILE* fptr = fopen(file1, "r");
   char* str = (char*) malloc(sizeof(char)*2048);
@@ -18,6 +21,15 @@ int* fileToArray(char file1[], int* n){
   }
  fclose(fptr);
  return array;
+}
+__global__
+void lastDigit(int* array, int* result, int n) {
+  
+  int index = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = blockDim.x * gridDim.x;
+  for (int i = index; i < n; i += stride) {
+    result[i] = array[i] % 10;
+  }
 }
 
 __global__
@@ -36,14 +48,12 @@ void min(int* array, int n){
 
 int computeMin(int* array, int n){
 
-  int numThreads = 1;
-  int numBlocks = 1;
-  min<<<numBlocks, numThreads>>>(array, n);
+  min<<<NUM_BLOCKS_A, NUM_THREADS_A>>>(array, n);
 
   cudaDeviceSynchronize();
 
   int minNum = INT_MAX;
-  for(int i = 0; i < numThreads; i++){
+  for(int i = 0; i < NUM_THREADS_A; i++){
     if(array[i] < minNum){
       minNum = array[i];
     }
@@ -51,13 +61,24 @@ int computeMin(int* array, int n){
   return minNum;
 }
 
+void computeLastDigit(int* array, int n) {
+  int* result;
+  cudaMallocManaged(&result, sizeof(int)*(n));
+  lastDigit<<<NUM_BLOCKS_B, NUM_THREADS_B>>>(array, result, n);
+
+  cudaDeviceSynchronize();
+  for (int i = 0; i < 10; i++) {
+    printf("array[%d]: %d, result[%d]: %d\n", i, array[i], i, result[i]);
+  }
+}
 
 int main(int argc, char* argv[]){
   int n;
   int* array = fileToArray("inp.txt", &n);
-  for(int i = 0; i < 10; i++){
+  /*for(int i = 0; i < 10; i++){
     printf("%d\n", array[i]);
-  }
+  }*/
+  computeLastDigit(array, n);
   int min = computeMin(array, n);
   printf("min: %d\n", min);
   cudaFree(array);
