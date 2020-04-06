ff78aebd84328ae4016444171e08d3572463499f
Geoffrey Tian <gtian@login1.maverick2.tacc.utexas.edu>
2020-04-05 21:01:16 -0500

    outputs number of odds

--- make_inp.py
+++ make_inp.py
@@ -2,14 +2,18 @@ import random
 random.seed(a = None, version=2)
 f = open("inp.txt", "w+")
 f.write("1000000,")
-x = [0] * 10;
+x = [0] * 10
+odds = 0
 for i in range(1000000):
 	k = random.randint(0, 999)
 	x[int(k/100)] += 1
-	f.write(str(k))
+	if (k % 2 == 1):
+		odds += 1	
+        f.write(str(k))
 	f.write(",")
 f.write("900")
 x[9] += 1
 
 for i in range(10):
-	print("x[i]: " + str(x[i]))
+	print("x[" + i + "]: " + str(x[i]))
+print("# of odds: " + odds)
